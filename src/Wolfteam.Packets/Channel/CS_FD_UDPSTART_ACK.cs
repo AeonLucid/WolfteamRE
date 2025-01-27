4cf36cb78bbe8c075445f08d50919581456933f4
AeonLucid <aeonlucid@gmail.com>
2025-01-27 09:16:25 +0100

    Add udp start packet

---
+++ src/Wolfteam.Packets/Channel/CS_FD_UDPSTART_ACK.cs
@@ -0,0 +1,9 @@
+﻿using Wolfteam.Packets.Attributes;
+
+namespace Wolfteam.Packets.Channel;
+
+[WolfteamPacket(PacketId.CS_FD_UDPSTART_ACK)]
+public partial class CS_FD_UDPSTART_ACK : IWolfPacket
+{
+    public byte Uk1 { get; set; }
+}
---
+++ src/Wolfteam.Packets/Channel/CS_FD_UDPSTART_REQ.cs
@@ -0,0 +1,9 @@
+﻿using Wolfteam.Packets.Attributes;
+
+namespace Wolfteam.Packets.Channel;
+
+[WolfteamPacket(PacketId.CS_FD_UDPSTART_REQ)]
+public partial class CS_FD_UDPSTART_REQ : IWolfPacket
+{
+    
+}
